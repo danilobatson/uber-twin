85f10f9722a7e38856aecf7648fb0e88f7ec103f
danilobatson <djbatson19@gmail.com>
2023-04-20 21:07:59 -0700

    adds HomeScreen

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 
 import store from './store';
+import { HomeScreen } from './screens';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber build to come</Text>
+      <SafeAreaView style={styles.container}>
+        <HomeScreen />
         <StatusBar style='auto' />
-      </View>
+      </SafeAreaView>
     </Provider>
   );
 }
--- store/slices/navSlice.js
+++ store/slices/navSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    origin: null,
-    destination: null,
-    travelTimeInformation: null,
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
 };
 
 export const navSlice = createSlice({
@@ -23,12 +23,16 @@ export const navSlice = createSlice({
 });
 
 //ACTIONS - This is how we send the data into the data layer
-export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
 
 //SELECTORS - This is how we pull information from the Global store slice
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
-export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
 export const selectNav = (state) => state.nav;
+
+export const { origin, destination, travelTimeInformation } = selectNav;
 
 export default navSlice.reducer;

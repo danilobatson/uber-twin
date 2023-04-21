8c8df857e379f9093cf725c08e7104e0b18533e2
danilobatson <djbatson19@gmail.com>
2023-04-20 20:47:37 -0700

    adds react redux

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (

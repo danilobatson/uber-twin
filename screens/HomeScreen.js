2689412a001cc1d2d3ad5a9e6d9c5c298c6a1fed
danilobatson <djbatson19@gmail.com>
2023-04-20 21:15:53 -0700

    adds uber image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png',
+          }}
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+        />
+      </View>
       <Text style={tw`text-red-500 p-10`}>HomeScreen</Text>
     </SafeAreaView>
   );

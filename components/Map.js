03cfe13e26e510ac6b132c4cbfb4f7b5a07d33c2
danilobatson <djbatson19@gmail.com>
2023-05-11 14:24:28 -0700

    adds auto zoom on origin and destination

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import MapView, { Marker } from 'react-native-maps';
 import { useSelector } from 'react-redux';
@@ -10,9 +10,22 @@ import { GOOGLE_MAPS_API_KEY } from '@env';
 const Map = () => {
   const { origin, destination } = useSelector(selectNavState);
 
+  const mapRef = useRef();
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    // Zoom & fit to markers
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      animated: true,
+    });
+  }, [origin, destination]);
+
   return (
     <>
       <MapView
+        ref={mapRef}
         style={tw`flex-1`}
         mapType='mutedStandard'
         initialRegion={{
@@ -40,6 +53,17 @@ const Map = () => {
             title='Origin'
             description={origin.description}
             identifier='origin'
+          />
+        )}
+        {destination?.location && (
+          <Marker
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            title='Destination'
+            description={destination.description}
+            identifier='destination'
           />
         )}
       </MapView>
